d6f19eb2538a6912255b729c241e3ced5cf5c5e3
Omar.Aljaber <omar.aljaber@proplant-mvi.com>
2025-07-22 10:17:37 +0200

    create Controller and Model for Projects, and add create-view to create projects

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(ProfileController::class)->middleware('auth')->group(function () {
+    Route::get('/profile', 'edit')->name('profile.edit');
+    Route::patch('/profile', 'update')->name('profile.update');
+    Route::delete('/profile', 'destroy')->name('profile.destroy');
+});
+
+Route::controller(ProjectController::class)->middleware('auth')->group(function () {
+    Route::get('/projects/create', 'create')->name('projects.create');
+    Route::post('/projects/create', 'store')->name('projects.store');
 });
 
 require __DIR__.'/auth.php';
